9d98d2d2b8f2669f9fbb6f108b8e2d5cf2bd1074
Paul Larin <103615207+paullarinweb@users.noreply.github.com>
2022-04-18 17:56:47 +0800

    Add files via upload
    Just page, create with tailwind css

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 300,
+  // autoplay: true,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
